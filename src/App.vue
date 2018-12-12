921adc33383537004fdcd33d7b7748d9aec0dd4e
MASATO ARAI <ponponsippo@gmail.com>
2018-12-12 15:58:17 +0900

    henkou

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    
-    <section>
-        <button class="button is-medium is-dark" @click="prompt">
-            Launch prompt (default)
-        </button>
-
-        <button class="button is-medium is-dark" @click="promptNumber">
-            Launch prompt (number)
-        </button>
-    </section>
-      
-    <button type="button"  class="button is-medium is-dark" @click="open">Open Modal</button>
+    <button type="button"  class="button is-medium is-dark" @click="open">現在地を取得しMapfunで見る</button>
+      <b-input v-model="gatLongLat" placeholder="緯度経度の自動取得" disabled></b-input>
     <modal name="example">
       <div class="basic-modal">
+         <b-notification type="is-danger" v-if="failGps">
+            GPS情報の取得に失敗しました
+        </b-notification>
+        <section v-show="!failGps">
         <iframe id="mm" src=""></iframe>
-        <div id="show_result"></div>
-        <button class="button" type="button" @click="close">Close Modal</button>
+        <div id="show_result">あなたの居る座標は{{showResult}}</div>
+            <button type="button"  class="button is-primary" @click="googlemap">googleMAPで見る[別窓]</button>
+        </section>
+        <button class="button is-dark" type="button" @click="close">Close</button>
+          
       </div>
+        
     </modal>
+         <b-notification type="is-danger" v-if="!canGeolocation">
+            GPS情報の取得機能がありません。
+        </b-notification>
   </div>
 </template>
 
@@ -28,65 +29,54 @@
     
 export default {
   name: 'app',
+    data:function(){
+        return{
+            canGeolocation:true,
+            long:0.0,
+            lat:0.0,
+            failGps:false,
+            gatGps:false
+        }
+    },
+    computed:{
+        showResult:function(){
+            return `緯度:${this.long},経度:${this.lat}`;
+        },
+        gatLongLat:function(){
+           return this.gatGps?`${this.long},${this.lat}`:'';
+        }
+    },
   components: {
    // HelloWorld
   },
     methods: {
-        prompt() {
-            navigator.geolocation.getCurrentPosition(
-successCallback, errorCallback
-);
-            function successCallback(position) {
-  var gl_text = "緯度：" + position.coords.latitude + "<br>";
-    gl_text += "経度：" + position.coords.longitude + "<br>";
-    gl_text += "高度：" + position.coords.altitude + "<br>";
-    gl_text += "緯度・経度の誤差：" + position.coords.accuracy + "<br>";
-    gl_text += "高度の誤差：" + position.coords.altitudeAccuracy + "<br>";
-    gl_text += "方角：" + position.coords.heading + "<br>";
-    gl_text += "速度：" + position.coords.speed + "<br>";
-  document.getElementById("show_result").innerHTML = gl_text;
-}
-            function errorCallback(e){
-                console.log(e);
-            }
-            this.$dialog.prompt({
-                message: `What's your name?`,
-                inputAttrs: {
-                    placeholder: 'e.g. Walter',
-                    maxlength: 10
-                },
-                onConfirm: (value) => this.$toast.open(`Your name is: ${value}`)
-            })
+        googlemap () {
+            let line = `https://www.google.com/maps/@${this.long},${this.lat}`;
+            window.open(line);
         },
-        promptNumber() {
-            this.$dialog.prompt({
-                message: `What's your age?`,
-                inputAttrs: {
-                    type: 'number',
-                    placeholder: 'Type your age',
-                    value: '18',
-                    maxlength: 2,
-                    min: 18
-                },
-                onConfirm: (value) => this.$toast.open(`Your age is: ${value}`)
-            })
-        },
-        
         open () {
+            if(navigator.geolocation){
+                this.canGeolocation=true;
+            }else{
+                this.canGeolocation = false;
+                return false;
+            }
+            
           this.$modal.push('example');
+            let self = this;
             navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
             function successCallback(position) {
-              var gl_text = "緯度：" + position.coords.latitude + "<br>";
-                gl_text += "経度：" + position.coords.longitude;
-              document.getElementById("show_result").innerHTML = gl_text;
-            let dl = document.getElementById('mm').src="https://mapfan.com/map?c="+position.coords.latitude+","+position.coords.longitude+",15";
-            console.log(dl);
+                self.lat = position.coords.latitude;
+                self.long = position.coords.longitude;
+                document.getElementById('mm').src="https://mapfan.com/map?c="+position.coords.latitude+","+position.coords.longitude+",15";
+                    self.failGps = false;
+                self.gatGps = true;
             }
                 function errorCallback(e){
+                    self.failGps = true;
                     console.log(e);
                 }
         },
-
         close () {
           this.$modal.pop()
         }
@@ -95,17 +85,32 @@ successCallback, errorCallback
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
     #mm{
-        width: 100%;
-        height: 80vw;
+        width: 98vw;
+        height: 70vh;
         border: 1px solid black;
+    }
+    .modal-content {
+    position: absolute;
+    top: 0 !important;
+    height: 100vh !important;
+    left: 0 !important;
+    right: 0 !important;
+    opacity: 1;
+    margin: 0 auto !important;
+    padding: 1vw !important;
+    width: 100vw !important;
+    max-height: 100vh !important;
+    background-color: #fff;
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
     }
 </style>
